1d68338d70c1589b5bc247470cb102f06879bf2a
Zihadul Islam <zihadul10101@gmail.com>
2022-01-13 10:52:29 +0600

    Convert web To App Done

--- app/src/main/java/com/zihadul/practicechallenge4/MainActivity.java
+++ app/src/main/java/com/zihadul/practicechallenge4/MainActivity.java
@@ -3,12 +3,30 @@ package com.zihadul.practicechallenge4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout webToApp;
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webToApp = findViewById(R.id.webToApp);
+        mWebView = findViewById(R.id.mWebView);
+
+        webToApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebView.getSettings().setJavaScriptEnabled(true);
+                mWebView.loadUrl("https://internshala.com/");
+            }
+        });
+
     }
 }
